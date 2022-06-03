8b9eab5d35a596fdc7266af2bd683e5ba6f7cb9a
Wilku <arek.szumacher@gmail.com>
2022-06-03 09:02:41 +0200

    add table, fix query, ad info about loader

--- src/SearchGit.js
+++ src/SearchGit.js
@@ -4,6 +4,7 @@ import Select from 'react-select'
 export default class SearchGit extends React.Component {
 
     options = [
+        { value: '', label: '' },
         { value: 'user', label: 'User' },
         { value: 'repository', label: 'Repository' },
       ]
@@ -11,7 +12,7 @@ export default class SearchGit extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = { value: '', type: '' };
+        this.state = { value: '', type: '', loader: false , data: []};
 
         this.searchValue = this.searchValue.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -21,12 +22,16 @@ export default class SearchGit extends React.Component {
     searchValue(event) {
         console.warn(this.state)
         event.preventDefault();
+        this.setState({loader: true});
         fetch(this.getSearchUrl())
             .then(response => response.json())
             .then(res => {
                 console.warn(res)
+                this.setState({data: res.items})
             }).catch(e => {
                 console.error(e)
+            }).finally(() => {
+                this.disableLoader()
             })
     }
 
@@ -39,10 +44,25 @@ export default class SearchGit extends React.Component {
         this.setState({ type: event.target.value })
     }
 
+    disableLoader(){
+        this.setState({loader: false});
+    }
+
     getSearchUrl() {
         //TODO change if select changed
-        return `https://api.github.com/${this.state.type === 'user' ? `users/${this.state.value}` : `search/repositories?q=${this.state.value}&1,50,sort,order`}`
+        return `https://api.github.com/${this.state.type === 'user' ? `search/users?q=${this.state.value}` : `search/repositories?q=${this.state.value}&1,50,sort,order`}`
 
+    }
+    logSstate(){
+        console.warn(this.state)
+    }
+
+    renderTable(){
+        
+    }
+
+    isValid(){
+        return !(!!this.state.type && this.state.value.length > 2) 
     }
 
     render() {
@@ -54,8 +74,30 @@ export default class SearchGit extends React.Component {
                     <select value={this.state.type} onChange={this.selectChange}>
                       {this.options.map(e => <option value={e.value} key={e.label}>{e.label}</option>)}
                     </select>
-                    <button type="submit">Search</button>
+                    <button type="submit" disabled={this.isValid()}>Search</button>
                 </form>
+               {    this.state.loader ?<div><h2>Loading data</h2></div> : null}
+               {
+               this.state.data.length ? 
+               <div>
+               <table>
+                   <thead>
+                      <tr>
+                       <th>User name</th>
+                       <th>Repository name</th>
+                       </tr>   
+                       </thead>
+                       <tbody>
+                           {this.state.data.map(e =>{ return <tr>
+                               <td>{e.login}</td>
+                               <td>{e.repository}</td>
+
+                           </tr>})}
+                           </tbody> 
+               </table>
+               </div>
+               : <div>No data found</div>
+                }
             </div>
         );
     }
