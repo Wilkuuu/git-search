d28778feb6f8b9b42977037b2d8db01c1ae45c51
Wilku <arek.szumacher@gmail.com>
2022-06-03 09:17:21 +0200

    ann error template

--- src/SearchGit.js
+++ src/SearchGit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Select from 'react-select'
 
 export default class SearchGit extends React.Component {
@@ -7,12 +7,12 @@ export default class SearchGit extends React.Component {
         { value: '', label: '' },
         { value: 'user', label: 'User' },
         { value: 'repository', label: 'Repository' },
-      ]
+    ]
 
 
     constructor(props) {
         super(props)
-        this.state = { value: '', type: '', loader: false , data: []};
+        this.state = { value: '', type: '', loader: false, data: [], error: false };
 
         this.searchValue = this.searchValue.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -22,14 +22,18 @@ export default class SearchGit extends React.Component {
     searchValue(event) {
         console.warn(this.state)
         event.preventDefault();
-        this.setState({loader: true});
+        this.setState({ loader: true });
         fetch(this.getSearchUrl())
-            .then(response => response.json())
+            .then(response => response.json()).catch(e => {
+                console.error('MY EERROR', e)
+                this.setState({ error: e })
+            })
             .then(res => {
                 console.warn(res)
-                this.setState({data: res.items})
+                this.setState({ data: res.items })
             }).catch(e => {
-                console.error(e)
+                console.error('MY EERROR', e)
+                this.setState({ error: e })
             }).finally(() => {
                 this.disableLoader()
             })
@@ -44,25 +48,24 @@ export default class SearchGit extends React.Component {
         this.setState({ type: event.target.value })
     }
 
-    disableLoader(){
-        this.setState({loader: false});
+    disableLoader() {
+        this.setState({ loader: false });
     }
 
     getSearchUrl() {
-        //TODO change if select changed
         return `https://api.github.com/${this.state.type === 'user' ? `search/users?q=${this.state.value}` : `search/repositories?q=${this.state.value}&1,50,sort,order`}`
 
     }
-    logSstate(){
+    logSstate() {
         console.warn(this.state)
     }
 
-    renderTable(){
-        
+    renderTable() {
+
     }
 
-    isValid(){
-        return !(!!this.state.type && this.state.value.length > 2) 
+    isValid() {
+        return !(!!this.state.type && this.state.value.length > 2)
     }
 
     render() {
@@ -72,31 +75,42 @@ export default class SearchGit extends React.Component {
                 <form onSubmit={this.searchValue}>
                     <input type="text" value={this.state.value} onChange={this.handleChange} ></input>
                     <select value={this.state.type} onChange={this.selectChange}>
-                      {this.options.map(e => <option value={e.value} key={e.label}>{e.label}</option>)}
+                        {this.options.map(e => <option value={e.value} key={e.label}>{e.label}</option>)}
                     </select>
                     <button type="submit" disabled={this.isValid()}>Search</button>
                 </form>
-               {    this.state.loader ?<div><h2>Loading data</h2></div> : null}
-               {
-               this.state.data.length ? 
-               <div>
-               <table>
-                   <thead>
-                      <tr>
-                       <th>User name</th>
-                       <th>Repository name</th>
-                       </tr>   
-                       </thead>
-                       <tbody>
-                           {this.state.data.map(e =>{ return <tr>
-                               <td>{e.login}</td>
-                               <td>{e.repository}</td>
+                {this.state.loader ? <div><h2>Loading data</h2></div> : null}
+                {
+                    this.state.data.length ?
+                        <div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>User name</th>
+                                        <th>Repository name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.data.map(e => {
+                                        return <tr>
+                                            <td>{e.login}</td>
+                                            <td>{e.repository}</td>
 
-                           </tr>})}
-                           </tbody> 
-               </table>
-               </div>
-               : <div>No data found</div>
+                                        </tr>
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                        : <div>No data found</div>
+                }
+                {
+                    this.state.error ?
+                        <div>
+                            <h2>Error</h2>
+                            <div>{this.state.error.message}</div>
+                        </div>
+                        :
+                        null
                 }
             </div>
         );
